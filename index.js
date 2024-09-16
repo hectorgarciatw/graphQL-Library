b1c333c19288308ddb9baefcb3eafbc7984ba33a
hectorgarciatw <dreamallica@gmail.com>
2024-09-16 14:50:51 -0300

    Implementation exercises of part8 (Exercises 8.16-8.17)

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const connectDB = require("./db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Book = require("./models/Book");
 const Author = require("./models/Author");
 const User = require("./models/User");
 const { GraphQLError } = require("graphql");
+
+const JWT_SECRET = process.env.JWT_SECRET;
 require("dotenv").config();
 
 connectDB();
@@ -25,7 +29,18 @@ const typeDefs = `
         genres: [String!]!
     }
 
+    type User {
+        username: String!
+        favoriteGenre: String!
+        id: ID!
+    }
+
+    type Token {
+        value: String!
+    }
+
     type Query {
+        me: User
         bookCount: Int!
         authorCount: Int!
         allBooks(author: String, genre: String): [Book!]!
@@ -41,6 +56,17 @@ const typeDefs = `
         ): Book
 
         editAuthor(name: String!, setBornTo: Int!): Author
+
+        createUser(
+            username: String!
+            favoriteGenre: String!
+            password: String!
+        ): User
+
+        login(
+            username: String!
+            password: String!
+        ): Token
     }
 `;
 
@@ -65,60 +91,105 @@ const resolvers = {
             return Book.find(query).populate("author");
         },
         allAuthors: async () => Author.find({}),
+        me: (root, args, context) => {
+            return context.currentUser; // Usuario actual basado en el token JWT
+        },
+    },
+    Mutation: {
+        addBook: async (root, args, context) => {
+            if (!context.currentUser) {
+                throw new GraphQLError("No autorizado", {
+                    extensions: { code: "UNAUTHENTICATED" },
+                });
+            }
+
+            let author = await Author.findOne({ name: args.author });
+
+            if (!author) {
+                author = new Author({ name: args.author });
+                await author.save();
+            }
+
+            const newBook = new Book({
+                title: args.title,
+                published: args.published,
+                author: author._id,
+                genres: args.genres,
+            });
+
+            await newBook.save();
+            return newBook.populate("author");
+        },
+
+        editAuthor: async (root, args, context) => {
+            if (!context.currentUser) {
+                throw new GraphQLError("No autorizado", {
+                    extensions: { code: "UNAUTHENTICATED" },
+                });
+            }
+
+            const author = await Author.findOne({ name: args.name });
+            if (!author) {
+                throw new GraphQLError("El autor no existe", {
+                    extensions: { code: "NOT_FOUND" },
+                });
+            }
+
+            author.born = args.setBornTo;
+            await author.save();
+            return author;
+        },
+
+        createUser: async (root, args) => {
+            const { username, favoriteGenre } = args;
+
+            const existingUser = await User.findOne({ username });
+            if (existingUser) {
+                throw new GraphQLError("El nombre de usuario ya está en uso", {
+                    extensions: { code: "BAD_USER_INPUT" },
+                });
+            }
+
+            const passwordHash = await bcrypt.hash("defaultpassword", 10); // Contraseña predeterminada
+
+            const newUser = new User({
+                username,
+                favoriteGenre,
+                passwordHash,
+            });
+
+            try {
+                await newUser.save();
+                return newUser;
+            } catch (error) {
+                throw new GraphQLError("Error al crear el usuario: " + error.message, {
+                    extensions: { code: "BAD_USER_INPUT" },
+                });
+            }
+        },
+
+        login: async (root, args) => {
+            const { username, password } = args;
+            const user = await User.findOne({ username });
+            if (!user) {
+                throw new GraphQLError("Usuario no encontrado", {
+                    extensions: { code: "BAD_USER_INPUT" },
+                });
+            }
+
+            const passwordValid = await bcrypt.compare(password, user.passwordHash);
+            if (!passwordValid) {
+                throw new GraphQLError("Contraseña incorrecta", {
+                    extensions: { code: "BAD_USER_INPUT" },
+                });
+            }
+
+            const token = jwt.sign({ id: user._id, username: user.username }, JWT_SECRET);
+            return { value: token };
+        },
     },
     Author: {
         bookCount: async (root) => Book.countDocuments({ author: root._id }),
-    },
-    Mutation: {
-        addBook: async (root, args) => {
-            try {
-                let author = await Author.findOne({ name: args.author });
-
-                if (!author) {
-                    author = new Author({ name: args.author });
-                    await author.save();
-                }
-
-                const newBook = new Book({
-                    title: args.title,
-                    published: args.published,
-                    author: author._id,
-                    genres: args.genres,
-                });
-
-                await newBook.save();
-                return newBook.populate("author");
-            } catch (error) {
-                throw new GraphQLError("Error al agregar el libro: " + error.message, {
-                    extensions: {
-                        code: "BAD_USER_INPUT",
-                    },
-                });
-            }
-        },
-
-        editAuthor: async (root, args) => {
-            try {
-                const author = await Author.findOne({ name: args.name });
-                if (!author) {
-                    throw new GraphQLError("El autor no existe", {
-                        extensions: {
-                            code: "NOT_FOUND",
-                        },
-                    });
-                }
-
-                author.born = args.setBornTo;
-                await author.save();
-                return author;
-            } catch (error) {
-                throw new GraphQLError("Error al editar el autor: " + error.message, {
-                    extensions: {
-                        code: "BAD_USER_INPUT",
-                    },
-                });
-            }
-        },
     },
 };
 
@@ -130,6 +201,20 @@ const server = new ApolloServer({
 
 // Init the server
 startStandaloneServer(server, {
+    context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null;
+        if (auth && auth.toLowerCase().startsWith("bearer ")) {
+            const token = auth.substring(7);
+            try {
+                const decodedToken = jwt.verify(token, JWT_SECRET);
+                const currentUser = await User.findById(decodedToken.id);
+                return { currentUser };
+            } catch (err) {
+                console.log("Token inválido");
+            }
+        }
+        return null;
+    },
     listen: { port: 4000 },
 }).then(({ url }) => {
     console.log(`Server ready at ${url}`);
